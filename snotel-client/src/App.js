e030d64c11017e2db5b5b8ce10c027b1f27b878c
jackv545 <11428322+jackv545@users.noreply.github.com>
2020-05-30 15:39:07 -0600

    update routes

--- snotel-client/src/App.js
+++ snotel-client/src/App.js
@@ -1,32 +1,14 @@
-import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import React, { useState } from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 import { Tooltip, IconButton, CssBaseline } from '@material-ui/core';
 import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
 import { Brightness4, BrightnessHigh } from '@material-ui/icons';
 
-
-import Navigation from './components/Navigation';
-import Home from './components/Home';
-import { sendServerRequestWithBody } from './api/restfulAPI';
-import WorldMap from './components/WorldMap';
+import Snotel from './components/Snotel'
 
 function App() {
-    const [selectedStation, setSelectedStation] = useState(null);
-    const [stations, setStations] = useState([]);
-
     const [prefersDarkMode, setPrefersDarkMode] = useState(true);
-
-    useEffect(() => {
-        sendServerRequestWithBody({requestType: 'stations', requestVersion: 1})
-        .then((response => {
-            if (response.statusCode >= 200 && response.statusCode <= 299) {
-                setStations(response.body.stations)
-            } else {
-                console.error("Response code: ", response.statusCode, response.statusText);
-            }
-        }));
-    }, []);
 
     const theme = createMuiTheme({
         palette: {
@@ -44,8 +26,14 @@ function App() {
     });
 
     const darkModeButton = (
-        <Tooltip title="Toggle light/dark theme" aria-label="toggle light/dark theme" leaveDelay={100}>
-            <IconButton color="inherit" onClick={() => setPrefersDarkMode(prefersDarkMode => !prefersDarkMode)}>
+        <Tooltip
+            title="Toggle light/dark theme"
+            aria-label="toggle light/dark theme" leaveDelay={100}
+        >
+            <IconButton color="inherit"
+                onClick={() => setPrefersDarkMode(prefersDarkMode =>
+                    !prefersDarkMode)}
+            >
                 {prefersDarkMode ? <BrightnessHigh /> : <Brightness4 />}
             </IconButton>
         </Tooltip>
@@ -55,23 +43,10 @@ function App() {
         <ThemeProvider theme={theme}>
             <CssBaseline />
             <Router>
-                <Navigation
-                    prefersDarkMode={prefersDarkMode} darkModeButton={darkModeButton}
-                    selectedStation={selectedStation} setSelectedStation={setSelectedStation}
-                    stations={stations} theme={theme}
+                <Snotel
+                    prefersDarkMode={prefersDarkMode}
+                    darkModeButton={darkModeButton} theme={theme}
                 />
-                <Switch>
-                    <Route path="/worldMap">
-                        <WorldMap prefersDarkMode={prefersDarkMode}/>
-                    </Route>
-                    <Route path="/">
-                        <Home 
-                            theme={theme} prefersDarkMode={prefersDarkMode}
-                            selectedStation={selectedStation}
-                            setSelectedStation={setSelectedStation}
-                        />
-                    </Route>
-                </Switch>
             </Router>
         </ThemeProvider>
     );
--- snotel-client/src/components/StationMap.js
+++ snotel-client/src/components/StationMap.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import 'leaflet/dist/leaflet.css';
 import 'react-leaflet-markercluster/dist/styles.min.css';
@@ -8,82 +8,89 @@ import MarkerClusterGroup from 'react-leaflet-markercluster';
 
 import './LeafletCluster.css';
 
-export default function StationMap(props) {
-    const tileLayer = () => {
-        const tileURL = props.prefersDarkMode 
-            ? "https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png"
-            : "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png";
+const tileLayer = (prefersDarkMode) => {
+    const tileURL = prefersDarkMode 
+        ? "https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png"
+        : "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png";
 
-        return ( <TileLayer url={tileURL}/> );
-    }
+    return ( <TileLayer url={tileURL}/> );
+}
 
-    const icon = () => {
-        return (
-            new L.icon({
-                iconUrl: require(`../images/map-marker-${props.prefersDarkMode ? 'secondary' : 'red'}.png`),
-                iconAnchor: [18, 34],
-                popupAnchor: [0, -32],
-                shadowUrl: null
-            })
-        );
-    }
+const icon = (prefersDarkMode) => {
+    return (
+        new L.icon({
+            iconUrl: require(`../images/map-marker-${prefersDarkMode ? 'secondary' : 'red'}.png`),
+            iconAnchor: [18, 34],
+            popupAnchor: [0, -32],
+            shadowUrl: null
+        })
+    );
+}
 
-    const marker = (lat, lng, name, key) => {
-        return(
-            <Marker 
-                icon={icon(props.prefersDarkMode)} 
-                position={[lat, lng]} key={key}
-            >
-                <Popup>
-                    {name}
-                </Popup>
-            </Marker>
-        );
-    }
+const marker = (lat, lng, name, prefersDarkMode, key='1') => {
+    return(
+        <Marker 
+            icon={icon(prefersDarkMode)} 
+            position={[lat, lng]} key={key}
+        >
+            <Popup>
+                {name}
+            </Popup>
+        </Marker>
+    );
+}
 
-    const createCluster = cluster => {
+const mapProps = {
+    attributionControl: false,
+    style: { height: 500 }
+}
+
+export default class StationMap extends Component {
+    createCluster = cluster => {
         return L.divIcon({
             html: `<span>${cluster.getChildCount()}</span>`,
-            className: 'marker-cluster dark',
+            className: `marker-cluster ${this.props.prefersDarkMode ? 'dark' : 'light'}`,
             iconSize: L.point(40, 40, true),
         });
     }
 
-    const allStationMap = () => {
+    allStationMap = () => {
         return(
             <Map
                 bounds={[[70.37, -164.29], [32.92, -103.79]]}
-                attributionControl={false}
-                style={{ height: 500, maxwidth: 700 }}
+                {...mapProps}
             >
-                {tileLayer()}
+                {tileLayer(this.props.prefersDarkMode)}
                 <MarkerClusterGroup
-                    iconCreateFunction={createCluster}
-                    polygonOptions={{weight: 0, fill: false}}
+                    iconCreateFunction={this.createCluster}
+                    showCoverageOnHover={false}
+                    maxClusterRadius={70}
                 >
-                    {props.stations.map((station, i) => (
-                        marker(station.lat, station.lng, station.name, i)
+                    {this.props.stations.map((station, i) => (
+                        marker(station.lat, station.lng, station.name, 
+                            this.props.prefersDarkMode, i.toString())
                     ))}
                 </MarkerClusterGroup>
             </Map> 
         );
     }
 
-    const singleStationMap = () => {
-        const station = props.selectedStation;
+    singleStationMap = () => {
+        const station = this.props.selectedStation;
 
         return(
             <Map
                 center={[station.lat, station.lng]}
-                zoom={8}
-                attributionControl={false}
-                style={{ height: 500, maxwidth: 700 }}
+                zoom={8} {...mapProps}
             >
-                {tileLayer()}
-                {marker(station.lat, station.lng, station.name)}
+                {tileLayer(this.props.prefersDarkMode)}
+                {marker(station.lat, station.lng, station.name,
+                    this.props.prefersDarkMode)}
             </Map>
         );
     }
 
-    return(props.all ? allStationMap() : singleStationMap());
+    render() {
+        return(this.props.all ? this.allStationMap() : this.singleStationMap());
+    }
 }
