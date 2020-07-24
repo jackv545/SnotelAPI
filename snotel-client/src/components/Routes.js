8b75ae0da023dae88122a7101aab0526554e72ec
jackv545 <11428322+jackv545@users.noreply.github.com>
2020-07-24 15:28:31 -0600

    update component name

---
+++ snotel-client/src/components/Routes.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+import Navigation from './Navigation';
+import Home from './Home';
+import WorldMap from './WorldMap';
+import States from './States';
+import StateInfo from './StateInfo';
+import StationInfo from './StationInfo';
+import Contact from './margins/Contact';
+import Footer from './margins/Footer';
+import PageNotFound from './margins/PageNotFound';
+
+export default function Routes(props) {
+    return (
+        <>
+            <Navigation
+                prefersDarkMode={props.prefersDarkMode}
+                darkModeButton={props.darkModeButton}
+                setDarkModeFocus={props.setDarkModeFocus}
+            />
+            <Switch>
+                <Route exact path="/">
+                    <Home prefersDarkMode={props.prefersDarkMode}/>
+                </Route>
+                <Route exact path="/contact">
+                    <Contact prefersDarkMode={props.prefersDarkMode}/>
+                </Route>
+                <Route exact path="/location">
+                    <WorldMap prefersDarkMode={props.prefersDarkMode}/>
+                </Route>
+                <Route path="/location/:stationUrlName">
+                    <StationInfo prefersDarkMode={props.prefersDarkMode}/>
+                </Route>
+                <Route exact path="/explore">
+                    <States/>
+                </Route>
+                <Route path="/explore/:state">
+                    <StateInfo/>
+                </Route>
+                <Route path="*">
+                    <PageNotFound/>
+                </Route>
+            </Switch>
+            <Footer prefersDarkMode={props.prefersDarkMode}/>
+        </>
+    );
+}
