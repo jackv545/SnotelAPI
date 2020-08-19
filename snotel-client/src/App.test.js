4957446e27bbfddbaa266629b3fdc34fcd91dcfa
jackv545 <11428322+jackv545@users.noreply.github.com>
2020-08-19 09:33:59 -0600

    update tests

--- snotel-client/src/App.test.js
+++ snotel-client/src/App.test.js
@@ -1,8 +1,8 @@
-import React from 'react';
 import { render } from '@testing-library/react';
 
+import React from 'react';
 import App from './App';
 
 test('full app render', () => {
-    render(<App />);
+    render(<App/>);
 });
