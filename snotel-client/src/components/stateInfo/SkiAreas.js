a2acd037563d377c79c4e2c4c2496f9c81599ca8
jackv545 <11428322+jackv545@users.noreply.github.com>
2020-08-19 09:34:15 -0600

    add ski areas view

---
+++ snotel-client/src/components/stateInfo/SkiAreas.js
@@ -0,0 +1,63 @@
+import React, { useState, useEffect } from 'react';
+
+import { Grid, Card, CardContent, Typography } from '@material-ui/core';
+import { makeStyles } from '@material-ui/styles';
+
+import { sendServerRequest } from '../../api/restfulAPI';
+// import { commasInNumber } from '../../utils/NumberCommas';
+import ListSkeleton from './ListSkeleton';
+
+const useStyles = makeStyles((theme) => ({
+    name: {
+        fontWeight: 'bold'
+    }
+}));
+
+export default function SkiAreas(props) {
+    useEffect(() => {
+        if(props.stateName.length > 0) {
+            document.title = `Ski Areas | ${props.stateName} | Snotel`;
+        }
+        
+    }, [props.stateName]);
+
+    const [skiAreas, setSkiAreas] = useState([]);
+
+    //get stations from stateID
+    useEffect(() => {
+        let isMounted = true;
+
+        sendServerRequest(`skiAreas?region=${props.regionID}`)
+            .then((response => {
+                if (response.statusCode >= 200 && response.statusCode <= 299) {
+                    if(isMounted) {
+                        setSkiAreas(response.body.skiAreas);
+                    }
+                } else {
+                    console.error("Response code: ", response.statusCode, response.statusText);
+                }
+            })
+        );
+        return () => { isMounted = false };
+    }, [props.regionID]);
+
+    const classes = useStyles();
+    
+    return (
+        <Grid container spacing={1}>
+            {skiAreas.length > 0 
+            ? skiAreas.map((skiArea) => (
+                <Grid item xs={12} sm={12} md={6} key={skiArea.name}>
+                    <Card>
+                        <CardContent>
+                            <Typography variant="inherit" color="secondary" className={classes.name}>
+                                {skiArea.name}
+                            </Typography>
+                        </CardContent>
+                    </Card>
+                </Grid>
+            ))
+            : <ListSkeleton/>}
+        </Grid>
+    );
+}
---
+++ snotel-client/src/components/stateInfo/StateInfo.js
@@ -0,0 +1,253 @@
+import React, { useState, useEffect } from 'react';
+import { useParams, useLocation, Link as RouterLink } from 'react-router-dom';
+
+import {Container, Grid, Typography, Button, Menu, MenuItem, ButtonBase, useMediaQuery, 
+    Hidden } from '@material-ui/core';
+import { Skeleton } from '@material-ui/lab';
+import { makeStyles, useTheme } from '@material-ui/styles';
+import { fade } from '@material-ui/core/styles';
+import { Public, Sort, ArrowDropDown } from '@material-ui/icons';
+
+import SkiAreas from './SkiAreas';
+import BackcountryStations from './BackcountryStations';
+import { sendServerRequest } from '../../api/restfulAPI';
+
+const useStyles = makeStyles((theme) => ({
+    container: {
+        marginTop: theme.spacing(3)
+    },
+    link: {
+        fontWeight: 'bold',
+        '&:hover': {
+            textDecoration: 'none'
+        }
+    },
+    listOptions: {
+        position: 'sticky',
+        [theme.breakpoints.up('sm')]: {
+            //top: (AppBar height) - (Header height)
+            top: `calc(${theme.spacing(6)}px - ${theme.typography.h4.fontSize})`
+        }
+    },
+    label: {
+        textTransform: 'none',
+        justifyContent: 'left'
+    },
+    noTextTransform: {
+        textTransform: 'none',
+    },
+    endIcon: {
+        marginLeft: 'auto'
+    },
+    tab: {
+        ...theme.typography.button,
+        textTransform: 'none',
+        padding: '5px 15px',
+        width: '100%',
+        transition: theme.transitions.create(['background-color'], {
+            duration: theme.transitions.duration.short,
+        }),
+        borderRadius: theme.shape.borderRadius,
+        justifyContent: 'left',
+        [theme.breakpoints.down('xs')]: {
+            borderRadius: theme.spacing(2),
+            justifyContent: 'center'
+        },
+        border: `1px solid ${
+            theme.palette.type === 'light' 
+            ? 'rgba(0, 0, 0, 0.23)' : 'rgba(255, 255, 255, 0.23)'
+        }`,
+        '&:hover': {
+            backgroundColor: 
+                fade(theme.palette.text.primary, theme.palette.action.hoverOpacity),
+            // Reset on touch devices, it doesn't add specificity
+            '@media (hover: none)': {
+                backgroundColor: 'transparent',
+            }
+        }
+    },
+    mt1: {
+        marginTop: theme.spacing(1)
+    },
+    mt2: {
+        marginTop: theme.spacing(2)
+    },
+    mt2smUp: {
+        [theme.breakpoints.up('sm')]: {
+            marginTop: theme.spacing(2)
+        }
+    },
+}));
+
+// A custom hook that builds on useLocation to parse the query string
+function useQuery() {
+    return new URLSearchParams(useLocation().search);
+}
+
+export default function StateInfo(props) {
+    const viewOptions = {'ski-areas': 'Ski Areas', backcountry: 'Backcountry'};
+    const viewOptionKeys = Object.keys(viewOptions);
+    const [selectedView, setSelectedView] = useState(viewOptionKeys[0]);
+    
+    let query = useQuery();
+
+    useEffect(() => {
+        if(query.get('tab')) {
+            setSelectedView(query.get('tab'));
+        } else {
+            setSelectedView(viewOptionKeys[0]);
+        }
+    }, [query, viewOptionKeys]);
+
+    const classes = useStyles();
+    const theme = useTheme();
+
+    const viewTabs = (
+        viewOptionKeys.map((viewOption, i) => (
+            <Grid 
+                item xs={6} sm={12} key={viewOption} 
+                className={i === 0 ? classes.mt2smUp : null}
+            >
+                <ButtonBase 
+                    focusRipple classes={{root: classes.tab}} 
+                    component={RouterLink} to={`?tab=${viewOption}`}
+                    style={viewOption === selectedView ? { 
+                        backgroundColor: 
+                            fade(
+                                theme.palette.secondary.main, 
+                                theme.palette.action.activatedOpacity
+                            ),
+                        pointerEvents: 'none'
+                    } : null}
+                >
+                    {viewOptions[viewOption]}
+                </ButtonBase>
+            </Grid>
+        ))
+    );
+
+    const sortOptions = { none: 'None', alphabetical: 'A-Z', 
+        elevation: 'Elevation', snowDepth: 'Snowpack' };
+    const [selectedSort, setSelectedSort] = useState('alphabetical');
+
+    const [anchorEl, setAnchorEl] = useState(null);
+
+    const handleClick = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+
+    const handleMenuItemClick = (optionKey) => {
+        setSelectedSort(optionKey);
+        handleClose();
+    }
+
+    const smUp = useMediaQuery(theme => theme.breakpoints.up('sm'));
+
+    const sortSelect = (
+        <div className={classes.mt2smUp}>
+            <Hidden xsDown>
+                <Grid item xs>
+                    <Typography variant="caption">
+                        Sort by:
+                    </Typography>
+                </Grid>
+            </Hidden>
+            <Button
+                fullWidth variant="outlined" aria-controls="sort-menu"
+                aria-haspopup="true"
+                classes={{ label: classes.label, endIcon: classes.endIcon }}
+                onClick={handleClick} endIcon={<ArrowDropDown/>}
+                startIcon={smUp ? null : <Sort/>}
+            >
+                {sortOptions[selectedSort]}
+            </Button>
+            <Menu
+                id="sort-menu" anchorEl={anchorEl} keepMounted
+                open={Boolean(anchorEl)} onClose={handleClose}
+                getContentAnchorEl={null} 
+                anchorOrigin={{ vertical: "bottom", horizontal: "center" }}
+                transformOrigin={{ vertical: "top", horizontal: "center" }}
+            >
+                {Object.keys(sortOptions).map((optionKey) => (
+                    <MenuItem
+                        onClick={() => handleMenuItemClick(optionKey)}
+                        selected={optionKey === selectedSort} key={optionKey}
+                    >
+                        {sortOptions[optionKey]}
+                    </MenuItem>
+                ))}
+            </Menu>
+        </div>
+    );
+
+    let urlParams = useParams();
+
+    const mapButton = (
+        <Button
+            fullWidth variant="outlined" startIcon={<Public/>}
+            classes={{ label: smUp ? classes.label : classes.noTextTransform}} 
+            className={classes.mt2smUp}
+            component={RouterLink} to={`/map/${urlParams.state}`}
+        >
+            Switch to Map
+        </Button>
+    );
+
+    const [stateName, setStateName] = useState('');
+    const [regionID, setRegionID] = useState(0);
+
+    useEffect(() => {
+        sendServerRequest(`state?state=${urlParams.state}`)
+            .then((response => {
+                if (response.statusCode >= 200 && response.statusCode <= 299) {
+                    setStateName(response.body.stateName);
+                    setRegionID(response.body.region)
+                } else {
+                    console.error("Response code: ", response.statusCode, response.statusText);
+                }
+            }));
+    }, [urlParams.state]);
+
+
+    const listOptions = (
+        <Grid container spacing={1} className={classes.listOptions}>
+            <Grid item xs={12}>
+                <Typography variant="h4" component="h1">
+                    {stateName.length > 0 ? stateName : <Skeleton />}
+                </Typography>
+            </Grid>
+            {viewTabs}
+            {selectedView === viewOptionKeys[1]
+                && <Grid item xs={6} sm={12}>
+                    {sortSelect}
+                </Grid>
+            }
+            <Grid item xs={6} sm={12}>
+                {mapButton}
+            </Grid>
+        </Grid>
+    );
+
+    return (
+        <Container maxWidth="md" className={classes.container}>
+            <Grid container spacing={2}>
+                <Grid item xs={12} sm={4}>
+                    {listOptions}
+                </Grid>
+                <Grid item xs={12} sm={8}>
+                    {selectedView === viewOptionKeys[0] 
+                        ? <SkiAreas stateName={stateName} regionID={regionID}/>
+                        : <BackcountryStations 
+                            state={urlParams.state} stateName={stateName} regionID={regionID} 
+                            selectedSort={selectedSort}
+                        />
+                    }
+                </Grid>
+            </Grid>
+        </Container>
+    );
+}
